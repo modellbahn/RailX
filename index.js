b960385d02fbbece7939b093fb926fa711d315c1
Green_Lab <greencoder001@outlook.com>
2022-11-19 19:56:34 +0100

    Web Interface started

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const http = require('http')
 // file deepcode ignore HttpToHttps: I love HTTP
 const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server)
+const deb = require('debson')
+deb.folder(path.join(__dirname, '.DeBSON'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.use(express.static(path.join(__dirname, 'htdocs')))
 
 io.on('connection', (socket) => {
     // Client connected
