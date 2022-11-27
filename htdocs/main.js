211601b3404d8c85a6616e98c7d80338401633fc
Green_Lab <greencoder001@outlook.com>
2022-11-27 12:28:17 +0100

    Day Light Cycler is functional & synchronised

--- htdocs/main.js
+++ htdocs/main.js
@@ -2,3 +2,83 @@ const socket = io()
 socket.on('console-log', console.log)
 const deb = new DeBSON(socket)
 window.deb = deb
+
+;((async()=>{
+
+
+
+    // Time Cycler
+    const zeroFirst = e => (e.toString().length === 2 ? e.toString() : '0' + e.toString())
+    await deb.cat('timecycler').obj('time').watch((newTime) => {
+        $$('.timespan').text(newTime)
+        if (newTime.split(':')[0] >= 6 && newTime.split(':')[0] <= 19) {
+            // Day
+            $$('.timeicon').inner('<i class="fa-solid fa-sun"></i>')
+        } else {
+            // Night
+            $$('.timeicon').inner('<i class="fa-solid fa-moon"></i>')
+        }
+    })
+    let timeOnBoot = (await deb.cat('timecycler').obj('time').read()) || '12:00'
+    if (timeOnBoot.split(':')[0] >= 6 && timeOnBoot.split(':')[0] <= 19) {
+        // Day
+        $$('.timeicon').inner('<i class="fa-solid fa-sun"></i>')
+    } else {
+        // Night
+        $$('.timeicon').inner('<i class="fa-solid fa-moon"></i>')
+    }
+    $$('.timespan').text(timeOnBoot)
+    $$('.time-adjust .fa-circle-minus').on('click', async () => {
+        let currentTime = (await deb.cat('timecycler').obj('time').read()) || '12:00'
+        let [hours, minutes] = currentTime.split(':')
+        minutes = parseInt(minutes)
+        minutes -= 30
+        if (minutes < 0) {
+            minutes += 60
+            hours = parseInt(hours)
+            hours -= 1
+            if (hours < 0) {
+                hours = 23
+            }
+        }
+        hours = zeroFirst(hours)
+        minutes = zeroFirst(minutes)
+        const newTime = `${hours}:${minutes}`
+        await deb.cat('timecycler').obj('time').write(newTime)
+    })
+    $$('.time-adjust .fa-circle-plus').on('click', async () => {
+        let currentTime = (await deb.cat('timecycler').obj('time').read()) || '12:00'
+        let [hours, minutes] = currentTime.split(':')
+        minutes = parseInt(minutes)
+        minutes += 30
+        if (minutes >= 60) {
+            minutes -= 60
+            hours = parseInt(hours)
+            hours += 1
+            if (hours >= 24) {
+                hours -= 24
+            }
+        }
+        hours = zeroFirst(hours)
+        minutes = zeroFirst(minutes)
+        const newTime = `${hours}:${minutes}`
+        await deb.cat('timecycler').obj('time').write(newTime)
+    })
+    $$('.dntogglers .fa-sun').on('click', async () => {
+        await deb.cat('timecycler').obj('time').write('12:00')
+    })
+    $$('.dntogglers .fa-moon').on('click', async () => {
+        await deb.cat('timecycler').obj('time').write('22:00')
+    })
+    $$('.dntogglers .daytimecyclelocked').on('click', async () => {
+        const isLocked = (await deb.cat('timecycler').obj('daytimecyclelocked').read()) || false
+        await deb.cat('timecycler').obj('daytimecyclelocked').write(!isLocked)
+    })
+    await deb.cat('timecycler').obj('daytimecyclelocked').watch((isLocked) => {
+        $$('.daytimecyclelocked').inner(isLocked ? '<i class="fa-solid fa-lock"></i>' : '<i class="fa-solid fa-unlock"></i>')
+    })
+    const isLockedOnBoot = (await deb.cat('timecycler').obj('daytimecyclelocked').read()) || false
+    await deb.cat('timecycler').obj('daytimecyclelocked').write(isLockedOnBoot)
+
+
+})());
